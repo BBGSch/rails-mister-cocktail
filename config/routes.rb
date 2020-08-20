06990d95fbc6dc1e57805916d43c044e410d48ca
Bram Schevenels <bram.schevenels@gmail.com>
2020-08-20 15:01:41 +0200

    cocktails routes and controller finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #create
+  get "cocktails/new", to: "cocktails#new"
+  post "cocktails/", to: "cocktails#create"
+
+  #read
+  get "cocktails", to: "cocktails#index" 
+  get "cocktails/:id", to: "cocktails#show", as: :cocktail
+  
+  #update
+  get "cocktails/:id/edit", to: "cocktails#edit"
+  patch "cocktails/:id/", to: "cocktails#update"
+  #delete
+  delete "cocktails/:id/", to: "cocktails#destroy"
+
+
 end
